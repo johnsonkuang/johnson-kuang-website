b617853e030e049745fa1928c53cf345a26d1aa6
Johnson Kuang <johnsonkuang@outlook.com>
2018-09-02 22:39:18 -0700

    added git ignore

--- johnsonkuangwebsite/website/models.py
+++ johnsonkuangwebsite/website/models.py
@@ -1,5 +1,33 @@
 from django.db import models
 
+from website.utils.fileutils import UniquePathAndRename
+
 # Create your models here.
 class Banner(models.Model):
-    image = models.ImageField(upload_to='banners/')
+    image = models.ImageField(blank=True, upload_to=UniquePathAndRename('banner', True), max_length=255)
+
+class Video(models.Model):
+    name = models.CharField(max_length=500)
+    video = models.FileField(verbose_name='static_videos', upload_to='videos/')
+
+    #Separation of videos by page they are to be loaded with for future filtering
+    INDEX = "Index"
+    ABOUT = "About"
+    RESUME = "Resume"
+    VACATION = "Vacation"
+    PROJECTS = "Projects"
+    SANDBOX = "Sandbox"
+    
+    PAGE_CHOICES = (
+        (INDEX, INDEX),
+        (ABOUT, ABOUT),
+        (RESUME, RESUME),
+        (VACATION, VACATION),
+        (PROJECTS, PROJECTS),
+        (SANDBOX, SANDBOX),
+    )
+
+    page = models.CharField(max_length=50, choices = PAGE_CHOICES, null=True)
+
+    def __str__(self):
+        return self.name + ": " + str(self.video)
