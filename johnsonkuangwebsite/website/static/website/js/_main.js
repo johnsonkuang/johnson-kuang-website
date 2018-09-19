cffe391b998f6eed674822d3d0a0d9e04923f335
Johnson Kuang <johnsonkuang@outlook.com>
2018-09-18 21:51:45 -0700

    changed parallax to new animation 2.0

--- johnsonkuangwebsite/website/static/website/js/_main.js
+++ johnsonkuangwebsite/website/static/website/js/_main.js
@@ -1,58 +1,19 @@
 /*! viewportSize | Author: Tyson Matanich, 2013 | License: MIT */
 (function(n){n.viewportSize={},n.viewportSize.getHeight=function(){return t("Height")},n.viewportSize.getWidth=function(){return t("Width")};var t=function(t){var f,o=t.toLowerCase(),e=n.document,i=e.documentElement,r,u;return n["inner"+t]===undefined?f=i["client"+t]:n["inner"+t]!=i["client"+t]?(r=e.createElement("body"),r.id="vpw-test-b",r.style.cssText="overflow:scroll",u=e.createElement("div"),u.id="vpw-test-d",u.style.cssText="position:absolute;top:-1000px",u.innerHTML="<style>@media("+o+":"+i["client"+t]+"px){body#vpw-test-b div#vpw-test-d{"+o+":7px!important}}<\/style>",r.appendChild(u),i.insertBefore(r,e.head),f=u["offset"+t]==7?i["client"+t]:n["inner"+t],i.removeChild(r)):f=n["inner"+t],f}})(this);
 
-/**
- * How to create a parallax scrolling website
- * Author: Petr Tichy
- * URL: www.ihatetomatoes.net
- * Article URL: http://ihatetomatoes.net/how-to-create-a-parallax-scrolling-website/
- */
+$(document).ready(function () {
+	console.log("reached");
+	var $heightDiv = $('.about-slide-img');
+	$('.about-displacer-div').height($('.about-slide-img').scrollHeight);
+	console.log($heightDiv.height());
+});
+$(document).ready(function () {
+	var height = document.getElementById('#about-image').offsetHeight;
+	console.log(height);
+})
 
-( function( $ ) {
-	
-	// Setup variables
-	$window = $(window);
-	$slide = $('.homeSlide');
-	$slideTall = $('.homeSlideTall');
-	$slideTall2 = $('.homeSlideTall2');
-	$body = $('body');
-	
-    //FadeIn all sections   
-	$body.imagesLoaded( function() {
-		setTimeout(function() {
-		      
-		      // Resize sections
-		      adjustWindow();
-		      
-		      // Fade in sections
-			  $body.removeClass('loading').addClass('loaded');
-			  
-		}, 800);
+$(function(){
+		skrollr.init({
+		forceHeight: false
 	});
-	
-	function adjustWindow(){
-		
-		// Init Skrollr
-		var s = skrollr.init({
-			forceHeight: false
-		});
-		
-		// Get window size
-	    winH = $window.height();
-	    
-	    // Keep minimum height 550
-	    if(winH <= 550) {
-			winH = 550;
-		} 
-	    
-	    // Resize our slides
-	    $slide.height(winH);
-	    $slideTall.height(winH*2);
-	    $slideTall2.height(winH*2);
-	    
-	    // Refresh Skrollr after resizing our sections
-	    s.refresh($('.homeSlide'));
-	    
-	}
-		
-} )( jQuery );
+});
