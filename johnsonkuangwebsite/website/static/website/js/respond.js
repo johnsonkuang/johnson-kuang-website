adb7a60259203c7ad20ad154e6e8ec0858ed1258
Johnson Kuang <johnsonkuang@outlook.com>
2018-09-30 23:10:06 -0700

    successfully integrated parallax coding

--- johnsonkuangwebsite/website/static/website/js/respond.js
+++ johnsonkuangwebsite/website/static/website/js/respond.js
@@ -23,14 +23,14 @@ window.matchMedia = window.matchMedia || (function(doc, undefined){
       fakeBody = doc.createElement('body'),
       div      = doc.createElement('div');
   
-  div.id = 'mq-test-1';
+  div.id = 'mq-embeds-1';
   div.style.cssText = "position:absolute;top:-100em";
   fakeBody.style.background = "none";
   fakeBody.appendChild(div);
   
   return function(q){
     
-    div.innerHTML = '&shy;<style media="'+q+'"> #mq-test-1 { width: 42px; }</style>';
+    div.innerHTML = '&shy;<style media="'+q+'"> #mq-embeds-1 { width: 42px; }</style>';
     
     docElem.insertBefore(fakeBody, refNode);
     bool = div.offsetWidth == 42;  
