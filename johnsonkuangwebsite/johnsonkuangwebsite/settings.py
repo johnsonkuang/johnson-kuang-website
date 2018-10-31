c0e85d99c614932bba47927be5ee40b9d772204c
Johnson Kuang <johnsonkuang@outlook.com>
2018-10-30 17:12:31 -0700

    updated db settings

--- johnsonkuangwebsite/johnsonkuangwebsite/settings.py
+++ johnsonkuangwebsite/johnsonkuangwebsite/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'johnsonkuangwebsite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'myproject',
+        'NAME': 'devtest',
         'USER': 'johnson',
         'PASSWORD': 'P@ssw0rd',
         'HOST': 'kuang-ubuntu.westus.cloudapp.azure.com',
